32739621b3a0bf25235e095cf45c717e722cc63f
Marc Neudert <marc.neudert@gmail.com>
2013-09-28 20:08:33 +0200

    marks missed targets

--- lib/game/target.dart
+++ lib/game/target.dart
@@ -2,11 +2,13 @@ part of game;
 
 class Target
 {
-  int position = 800;
+  int position   = 800;
+  bool targetHit = false;
 
   int level;
   int startTime;
   int targetCode;
+  int targetSize;
   String targetChar;
 
   Target(this.level)
@@ -26,9 +28,40 @@ class Target
     position     = 800 - (moveTime / 10).toInt();
 
     layer.ctx.font = '20px monospace bold';
-    layer.ctx.fillStyle = 'rgb(250, 250, 250)';
+    layer.ctx.fillStyle = getFillStyle(layer);
     layer.ctx.textAlign = 'center';
     layer.ctx.fillText(targetChar, position, 23 + 37 * level);
+  }
+
+  String getFillStyle(RenderLayer layer)
+  {
+    if (null == targetSize) {
+      targetSize = layer.ctx.measureText(targetChar).width.toInt();
+    }
+
+    if (isIncoming(layer)) {
+      return 'rgb(250, 250, 250)';
+    }
+
+    if (!targetHit && !isAway(layer)) {
+      return 'rgb(250, 250, 250)';
+    }
+
+    if (targetHit) {
+      return 'rgb(0, 250, 0)';
+    } else {
+      return 'rgb(250, 0, 0)';
+    }
+  }
+
+  bool isAway(RenderLayer layer)
+  {
+    return position < 180 - targetSize;
+  }
+
+  bool isIncoming(RenderLayer layer)
+  {
+    return position > 300;
   }
 
   bool outOfBounds()
